34979c7e94223de78126b4442e116fd34fd513c0
jadeabrinica <jgabrinica.student@asiancollege.edu.ph>
2025-06-20 12:48:06 +0800

    FREECODE

---
+++ script.js
@@ -0,0 +1,71 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+
+
+s
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('actlet menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+
+
+// Typing Text Code
+
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Blockchain Developer', 'Web Designer', 'Photographer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });ive');
+}
+
+
+
+
+
+
+
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+const typed = new Typed('.multiple-text',  {
+    strings: ['Physical Fitness', 'Weight Gain', 'Strength Training', 'Fat Lose', 'Weightlifting', 'Running'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+ });
